ce0cb92faa7b139fe51c5f058a850e2c4f6d0596
Bob Mrosky <rmrosk1@lps.umd.edu>
2022-03-25 13:11:31 -0400

    reordered example gif and renamed display to disp to avoid jupyter notebook collisions

--- example.py
+++ example.py
@@ -7,13 +7,13 @@ displays = [sevenSeg.sevenSegDisplay(color) for color in colors]
 
 # for each color display, we want to demonstrate what each digit looks like
 output = []
-for display in displays:
-    # we only need to loop through the values as that is
-    # what the display accepts
-    for digit in sevenSeg.sevenSegDisplay.mapping.values():
-        output.append(display.display(digit))
+# we only need to loop through the values as that is
+# what the display accepts
+for digit in sevenSeg.sevenSegDisplay.mapping.values():
+    for disp in displays:
+        output.append(disp.display(digit))
 
 # can save all the images as a gif if you want
 output[0].save('output.gif', save_all=True,
                append_images=output[1:], optimize=True,
-               loop=0, duration=1000)
+               loop=0, duration=300)
